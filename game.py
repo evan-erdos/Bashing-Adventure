9fe23a6e8dcc760290c75bd57d593ae29398a667
metaf <taf34@pitt.edu>
2016-02-05 22:37:04 -0500

    sdf

--- game.py
+++ game.py
@@ -2,16 +2,16 @@ import argparse
 
 
 def move(args):
-    print "Moved to a place"
+    print("Moved to a place")
 
 
 def act(args):
-    print "Act in a way"
+    print("Act in a way")
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+def look(args):
+    print("looked")
+
+parser = argparse.ArgumentParser(description='Play the Game')
 
 subparsers = parser.add_subparsers()
 
@@ -24,6 +24,8 @@ actparse = subparsers.add_parser('act', help='act')
 actparse.add_argument('args', metavar='ARG', type=int, nargs='*',
                       help='everything else')
 actparse.set_defaults(func=act)
+
+lookparse = subparsers.add_parser('look', help='look')
 
 if __name__ == "__main__":
     args = parser.parse_args()

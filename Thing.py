84e2831432dd729d36dcc2a9ceb0b2dcba04c792
english5040 <ejwu@andrew.cmu.edu>
2016-02-05 23:26:27 -0500

    Implemented Item

--- Thing.py
+++ Thing.py
@@ -22,3 +22,18 @@ class Thing:
 		print(description)
 		return False
 
+class Item(Thing):
+	"""
+	`Item` : **`Thing`**
+
+	An `Item` is a Thing that can be taken and dropped.
+	"""
+
+        def __init__(self, arg):
+                super(Item, self).__init__(arg)
+
+        def take(args):
+                return False
+
+        def drop(args):
+                return False

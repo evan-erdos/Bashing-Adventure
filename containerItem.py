880f0a9da577bdd8cb2766792ebd498ae27d6b9e
english5040 <ejwu@andrew.cmu.edu>
2016-02-06 12:59:20 -0500

    fixed small error

--- containerItem.py
+++ containerItem.py
@@ -9,10 +9,12 @@ class ContainerItem(Item):
     `ContainerItem` : **`Item`**
 
     A ContainerItem is an item that can hold other items. Weight/volume limits not implemented.
+
+    Todo: Description should iterate through contained items.
     """
 
     def __init__(self, data):
-        super(Item, self).__init__(data)
+        super(ContainerItem, self).__init__(data)
 
     def putin(args):
         return False

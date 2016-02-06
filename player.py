3470c4d8575ae474d8c4638f4bb6f85999e47225
english5040 <ejwu@andrew.cmu.edu>
2016-02-06 03:51:55 -0500

    added move method to player

--- player.py
+++ player.py
@@ -10,9 +10,14 @@ class Player:
         return Player(doc)
 
 
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
 
-    def save(self,filename):
-        with open(filename,'w') as f:
-            yaml.dump(self.data,f)
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            yaml.dump(self.data, f)
+
+    def move(self, room): # room must be a string (for now)
+        data[room] = room
+
+        #Todo: disallow moving to rooms that are not connected

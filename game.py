7c91fd26e72385ab1b27416a95c20f05ba3d1b8c
Spencer Baugh <sbaugh@catern.com>
2016-02-05 22:03:07 -0500

    args: parse em

--- game.py
+++ game.py
@@ -2,11 +2,19 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
     parser.add_argument('--sum', dest='accumulate', action='store_const',
                         const=sum, default=max,
                         help='sum the integers (default: find the max)')
+
+    subparsers = parser.add_subparsers()
+
+    move = subparsers.add_parser('move', help='move somewhere')
+    act = subparsers.add_parser('act', help='act')
+
+    move.add_argument('args', metavar='ARG', type=int, nargs='+',
+                      help='everything else')
+    act.add_argument('args', metavar='ARG', type=int, nargs='+',
+                     help='everything else')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))

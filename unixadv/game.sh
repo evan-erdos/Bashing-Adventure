c3327ca2d1654776a254e4479981c49509e8903c
Spencer Baugh <sbaugh@catern.com>
2016-02-06 16:45:42 -0500

    add session support: ./game.sh -s

--- unixadv/game.sh
+++ unixadv/game.sh
@@ -2,12 +2,17 @@
 set -o nounset
 set -o errexit
 mydir=$(dirname $0)
-
 if [ $# -lt 1 ];
 then
     echo "Pass a subcommand";
     exit 0;
 fi
+
+if [[ $1 = -s ]];
+then
+    xargs -n 1 -d '\n' -I % sh -c "$0 %"
+fi
+
 
 subcmd=$1
 shift;

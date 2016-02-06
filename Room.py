925241eed0507bc7ad7082ee71409040b31bc0ba
metaf <taf34@pitt.edu>
2016-02-05 23:29:22 -0500

    start Room

---
+++ Room.py
@@ -0,0 +1,15 @@
+import Thing
+class Room(Thing):
+    """
+    `Room` : **`Thing`**
+
+    A room is something the player is in.  It has items.
+    """
+    def __init__(self, arg):
+        super(Room, self).__init__()
+        self.arg = arg
+
+
+    def Look(args):
+        print(description)
+        return False
